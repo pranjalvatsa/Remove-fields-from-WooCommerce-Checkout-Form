6efb17edfa7fe4f3b4b5597ef6a8ed9d910ffcc8
pranjalvatsa <pranjalvatsa86@gmail.com>
2020-09-16 14:21:49 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+//Add below function to your theme/child theme's functions.php
+
+/**
+ Remove all possible fields
+ **/
+function wc_remove_checkout_fields( $fields ) {
+
+    // Billing fields
+    unset( $fields['billing']['billing_company'] );
+    unset( $fields['billing']['billing_state'] );   
+    unset( $fields['billing']['billing_address_1'] );
+    unset( $fields['billing']['billing_address_2'] );
+    unset( $fields['billing']['billing_city'] );
+    unset( $fields['billing']['billing_postcode'] );
+
+    // Order fields
+    unset( $fields['order']['order_comments'] );
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
